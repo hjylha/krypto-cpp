a10f7473457cc30191f59149908597ccd0888425
Heikki Jylhä <hjjylh@gmail.com>
2025-06-27 20:42:57 +0300

    added windows scripts

---
+++ build_script.ps1
@@ -0,0 +1,1 @@
+g++ .\src\basic_functions.cpp .\src\file_operations.cpp .\src\codewordpuzzle.cpp .\src\codewordpuzzle_cli.cpp .\src\main.cpp -o .\krypto.exe
---
+++ run_tests.ps1
@@ -0,0 +1,5 @@
+g++ .\src\basic_functions.cpp .\src\file_operations.cpp .\src\codewordpuzzle.cpp .\src\codewordpuzzle_cli.cpp .\src\test_functions.cpp .\src\tests.cpp -o .\test_files\run_tests.exe
+
+cd .\test_files\
+.\run_tests.exe
+cd..
